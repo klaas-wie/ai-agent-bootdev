812783525493b9365bdbba059e57c95af12e1e2a
klaas-wie <k.wiersma44@gmail.com>
2025-08-05 11:53:16 +0200

    Working on CH2

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,1 +1,16 @@
+import os
+
 def get_files_info(working_directory, directory="."):
+    pass
+
+def validate_boundaries(directory):
+    print(f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
+
