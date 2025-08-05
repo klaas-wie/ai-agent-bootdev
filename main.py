be60cf33ecad2c06f4f790d6714c5499dfcb0e1b
klaas-wie <k.wiersma44@gmail.com>
2025-08-05 11:36:29 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import os, sys
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
+def main():
+
+    if len(sys.argv) == 1:
+        print("prompt not provided")
+        sys.exit(1)
+
+    if len(sys.argv) >= 2:
+        user_prompt = sys.argv[1]
+        messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+                    ]
+        response = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages)
+
+        if len(sys.argv) == 3:
+            if sys.argv[2] == "--verbose":
+                print(f"User prompt: {user_prompt}")
+                print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+                print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
+        print(response.text)
+
+if __name__ == "__main__":
+    main()
